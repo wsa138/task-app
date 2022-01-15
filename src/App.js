4f67db9d0ba7f682fa3bd1f6beb5f835a899b237
William Addesso <wsaddesso@gmail.com>
2022-01-15 13:56:39 -0500

    newTask is now a controlled component. addTask() now sets newTask back to an empty string.

--- src/App.js
+++ src/App.js
@@ -2,26 +2,25 @@ import React from 'react';
 import Overview from '../src/components/Overview';
 
 function App() {
-  const [taskArray, setTaskArray] = React.useState(['Task 1', 'Task 2']);
+  const [taskArray, setTaskArray] = React.useState([]);
   const [newTask, setNewTask] = React.useState('');
 
-  // This function
+  // Sets newTask state.
   function handleChange(event) {
     event.preventDefault();
     setNewTask(event.target.value);
   }
 
-  console.log(newTask);
-
-  //This function adds the new task to taskArray on click of"Add Task" button.
+  //Adds the new task to taskArray on click of"Add Task" button.
   function addTask(event) {
     event.preventDefault();
     setTaskArray((prevArray) => {
       return [...prevArray, newTask];
     });
+    setNewTask('');
   }
 
-  //This function clears all tasks
+  //Clears all tasks.
   function clearTasks() {
     setTaskArray([]);
   }
@@ -30,7 +29,12 @@ function App() {
     <div>
       <form>
         <h1>New Task:</h1>
-        <input type="text" onChange={handleChange} name="newTask" />
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleChange}
+          name="newTask"
+        />
         <button onClick={addTask}>Add Task</button>
         <button onClick={clearTasks}>Clear Tasks</button>
       </form>
